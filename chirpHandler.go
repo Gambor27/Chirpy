790445feca3fcc0a9ed0acdac09b49356b40c17c
Gambor27 <gambor27@gmail.com>
2024-05-03 15:16:00 -0700

    Adding sorting

--- chirpHandler.go
+++ chirpHandler.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
+	"sort"
 	"strconv"
+
+	"github.com/Gambor27/Chirpy/internal/database"
 )
 
 func (cfg *apiConfig) newChirp(w http.ResponseWriter, r *http.Request) {
@@ -59,6 +63,41 @@ func (cfg *apiConfig) readChirps(w http.ResponseWriter, r *http.Request) {
 		respondWithJSON(w, http.StatusOK, chirps[chirpNum])
 		return
 	}
+	authorID := r.URL.Query().Get("author_id")
+	sort := r.URL.Query().Get("sort")
+	if len(authorID) > 0 {
+		id, err := strconv.Atoi(authorID)
+		if err != nil {
+			jsonError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+		authorChirps := make(map[int]database.Chirp)
+		for _, chirp := range chirps {
+			if chirp.AuthorID == id {
+				authorChirps[chirp.ID] = chirp
+			}
+		}
+		if len(sort) > 0 {
+			sortedChirps, err := cfg.sortChirps(authorChirps, sort)
+			if err != nil {
+				jsonError(w, http.StatusNotFound, err.Error())
+				return
+			}
+			respondWithJSON(w, http.StatusOK, sortedChirps)
+			return
+		}
+		respondWithJSON(w, http.StatusOK, authorChirps)
+		return
+	}
+	if len(sort) > 0 {
+		sortedChirps, err := cfg.sortChirps(chirps, sort)
+		if err != nil {
+			jsonError(w, http.StatusNotFound, err.Error())
+			return
+		}
+		respondWithJSON(w, http.StatusOK, sortedChirps)
+		return
+	}
 	respondWithJSON(w, http.StatusOK, chirps)
 }
 
@@ -100,4 +139,33 @@ func (cfg *apiConfig) deleteChirp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	respondWithJSON(w, http.StatusOK, "Chirp Deleted")
+}
+
+func (cfg *apiConfig) sortChirps(chirps map[int]database.Chirp, toSort string) ([]database.Chirp, error) {
+	outputChirps := make([]database.Chirp, 0)
+	for _, chirp := range chirps {
+		outputChirps = append(outputChirps, chirp)
+	}
+	sortFunction, err := cfg.getSort(outputChirps, toSort)
+	if err != nil {
+		return make([]database.Chirp, 0), err
+	}
+	if len(outputChirps) > 0 {
+		sort.Slice(outputChirps, sortFunction)
+	}
+	return outputChirps, nil
+}
+
+func (cfg *apiConfig) getSort(chirps []database.Chirp, direction string) (func(i, j int) bool, error) {
+	if direction == "asc" {
+		return func(i int, j int) bool {
+			return chirps[i].ID < chirps[j].ID
+		}, nil
+	} else if direction == "desc" {
+		return func(i int, j int) bool {
+			return chirps[i].ID > chirps[j].ID
+		}, nil
+	} else {
+		return nil, errors.New("sort type not known")
+	}
 }
